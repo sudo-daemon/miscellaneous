65f7656084b16e6becd1e89a88a51fca5476dac4
sudo-daemon <76221621+sudo-daemon@users.noreply.github.com>
2021-03-10 12:41:27 +0530

    Add files via upload

---
+++ shell_scripts/download-msj-chem.sh
@@ -0,0 +1,81 @@
+# Downloads all MSJ chem videos and organizes them into topic-wise folders
+
+mkdir msj-chem-videos
+cd msj-chem-videos
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcpvel_4y5PNGVH0sPcUw3jd
+mkdir Stoichiometric_relationships
+mv *.mp4 ./Stoichiometric_relationships
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcpsdSh9KhF3UbiVBJRNYucU
+mkdir Atomic_structure
+mv *.mp4 ./Atomic_structure
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcr7cYXf4590-WBw9GqEcv7m
+mkdir Periodicity
+mv *.mp4 ./Periodicity
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcoPrqjUCWw0J8P3qVGKCVCr
+mkdir Bonding
+mv *.mp4 ./Bonding
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcrLIP4nOMyClZckgOPcHP5i
+mkdir Energetics_and_thermochemistry
+mv *.mp4 ./Energetics_and_thermochemistry
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcoksVPpwVvF1HrajnORcs7x
+mkdir Kinetics
+mv *.mp4 ./Kinetics
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcoWAFhxfQok2pSX4w4Jakxx
+mkdir Equilibrium
+mv *.mp4 ./Equilibrium
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcrHadiZUSenEq1GAoCsKO-e
+mkdir Acids_and_bases
+mv *.mp4 ./Acids_and_bases
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcqJ6mPWn23T7HaHUZoLW0jc
+mkdir Redox_processes
+mv *.mp4 ./Redox_processes
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcrUFaOitEXFcloyRlgqm9bs
+mkdir Organic_chemistry
+mv *.mp4 ./Organic_chemistry
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcou5_0QJX8W3KVWsAm2qxob
+mkdir Measurement_and_Data_processing
+mv *.mp4 ./Measurement_and_Data_processing
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcqgw8i0Osao5FH2TkdSJRV2
+mkdir Areas_of_difficulty_SL
+mv *.mp4 ./Areas_of_difficulty_SL
+
+
+
+youtube-dl -f mp4 --quiet -i PLluIsqNl4jcqSdTBb9s5tyqnKyiX8kKXg
+mkdir Experimental_work
+mv *.mp4 ./Experimental_work
+
+
+
